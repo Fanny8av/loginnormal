ad981d0caac3cf259a54a00427279dd269d94e0c
Fanny <fanny@mac-book-pro-fany.local>
2015-10-31 23:37:09 -0600

    Initial Commit

---
+++ login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Fanny on 31/10/15.
+//  Copyright © 2015 Fanny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
