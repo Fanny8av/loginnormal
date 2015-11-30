8e50519a6f286cdc373572ebc75b06ad3a6ec22c
Fanny8av <fannynimini@gmail.com>
2015-11-30 08:03:49 -0600

    LoginNormal

---
+++ login/SignInVC.swift
@@ -0,0 +1,45 @@
+//
+//  SignInVS.swift
+//  login
+//
+//  Created by Fanny on 01/11/15.
+//  Copyright © 2015 Fanny. All rights reserved.
+//
+
+import UIKit
+
+class SignInVS: UIViewController {
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignUpButton(sender: UIButton) {
+    }
+    @IBAction func gotologin(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- login/ViewController.swift
+++ login/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewDidAppear(animated: Bool) {
+        self.performSegueWithIdentifier("gotologin", sender: self)
+    }
 
+    @IBAction func logout(sender: UIButton) {
+        self.performSegueWithIdentifier("gotologin", sender: self)
+    }
 
 }
-
